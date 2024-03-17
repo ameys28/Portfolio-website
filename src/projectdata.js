0678f1beb0acf63b0ba6f2cf82d2f489a75e4b8c
ameys8 <ameysawant2806@gmail.com>
2024-03-17 21:56:59 +0530

    Added some projects

--- src/projectdata.js
+++ src/projectdata.js
@@ -1,31 +1,31 @@
 
 export default [
   {
-    name: "Tic-tac-toe Game",
+    name: "Traditional Wellness Website",
     description:
-      "A tic-tac-toe game made using javascript",
-    image: "./images/tic-tac-toe.png",
-    website: "https://ameys28.github.io/Tic-Tac-Toe-game/",
-    github: "https://github.com/ameys28/Tic-Tac-Toe-game.git",
-    tools: ["HTML5", "CSS", "Javasvript"],
+      "A website dedicated to promoting traditional wellness practices for modern lifestyles.",
+    image: "./images/wellness.png",
+    website: "https://newbiethon.vercel.app/",
+    github: "https://github.com/ameys28/Traditional-Wellness-Website.git",
+    tools: ["HTML5", "CSS", "Javascript", "API"],
   },
   {
-    name: "To-do List",
+    name: "Animal Welfare Website",
+    description:
+      "An online platform dedicated to promoting animal welfare through donation and adoption fostering a compassionate community for animal lovers.",
+    image: "./images/animal.png",
+    website: "https://ameys28.github.io/Animal-Welfare/",
+    github: "https://github.com/ameys28/Animal-Welfare.git",
+    tools: ["HTML5", "CSS"],
+  },
+  {
+    name: "To-do List ",
     description:
       "A simple to-do list application which allows users to manage their tasks effectively by adding, removing, and editing tasks.",
     image: "./images/to-do-list.png",
     website: "https://ameys28.github.io/To-do-list/",
     github: "https://github.com/ameys28/To-do-list.git",
     tools: ["HTML5", "CSS", "Javasvript"],
-  },
-  {
-    name: "Airbnb",
-    description:
-      "Airbnb clone using React js",
-    image: "./images/airbnb.png",
-    website: "https://airbnb-two-drab.vercel.app/",
-    github: "https://github.com/ameys28/airbnb.git",
-    tools: ["React.js"],
   },
   {
     name: "Personal Website",
